ee8a660adafab8b288d37bbff5d35ecadb7661ac
PClancyy <36280985+PClancyy@users.noreply.github.com>
2026-02-08 14:32:36 +0000

    camera loop fix

--- ocrtext_extraction/ocr_det.py
+++ ocrtext_extraction/ocr_det.py
@@ -1,21 +1,22 @@
 import easyocr
 import cv2
 
+# Create the reader ONCE
+reader = easyocr.Reader(['en'], gpu=False)
 
 def ocr_det(img):
-    # create a reader object and extract text from image
-    reader = easyocr.Reader(['en'], gpu=False)
     results = reader.readtext(img, paragraph=True)
-    # Create bounding box and display extracted text over the image
+
     t = ''
     for (bbox, text) in results:
         (tl, tr, br, bl) = bbox
         t += text + '\n'
+
         tl = (int(tl[0]), int(tl[1]))
-        tr = (int(tr[0]), int(tr[1]))
         br = (int(br[0]), int(br[1]))
-        bl = (int(bl[0]), int(bl[1]))
+
         cv2.rectangle(img, tl, br, (0, 0, 255), 2)
         cv2.putText(img, text, (tl[0], tl[1] - 5),
                     cv2.FONT_HERSHEY_PLAIN, 1, (0, 0, 255), 2)
+
     return img, t
