527bb4b35e3d65b8613453464ce343eba0bff93f
PClancyy <36280985+PClancyy@users.noreply.github.com>
2026-02-19 14:24:58 +0000

    unmatchrd compon

--- ocr-servo/servo-test.py
+++ ocr-servo/servo-test.py
@@ -1,17 +1,36 @@
+import time
+import board
+import busio
 from adafruit_pca9685 import PCA9685
-from board import SCL, SDA
-import busio, time
 
-i2c = busio.I2C(SCL, SDA)
+SERVO_CH = 0
+
+i2c = busio.I2C(board.SCL, board.SDA)
 pca = PCA9685(i2c)
 pca.frequency = 50
 
-def set_angle(ch, angle):
-    pulse = int(4096 * ((angle * 11) + 500) / 20000)
-    pca.channels[ch].duty_cycle = pulse
+print("Starting servo test on channel", SERVO_CH)
 
-while True:
-    set_angle(0, 60)
-    time.sleep(1)
-    set_angle(0, 120)
-    time.sleep(1)
+def set_pulse(us):
+    period_us = 20000
+    duty = int(us / period_us * 65535)
+    pca.channels[SERVO_CH].duty_cycle = duty
+    print(f"Pulse: {us}us  Duty: {duty}")
+
+try:
+    while True:
+        print("Center")
+        set_pulse(1500)
+        time.sleep(1)
+
+        print("Left")
+        set_pulse(1000)
+        time.sleep(1)
+
+        print("Right")
+        set_pulse(2000)
+        time.sleep(1)
+
+except KeyboardInterrupt:
+    print("Stopping")
+    pca.channels[SERVO_CH].duty_cycle = 0
